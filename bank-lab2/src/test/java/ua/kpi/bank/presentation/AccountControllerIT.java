a2f59114a703a1bb847e40bdca51d1f0139d2b50
ykmshhh <yakukhno.maria@lll.kpi.ua>
2026-04-26 16:40:40 +0300

    lab2: test(presentation): add deposit and withdraw integration tests

--- bank-lab2/src/test/java/ua/kpi/bank/presentation/AccountControllerIT.java
+++ bank-lab2/src/test/java/ua/kpi/bank/presentation/AccountControllerIT.java
@@ -1,5 +1,6 @@
 package ua.kpi.bank.presentation;
 
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,6 +32,72 @@ class AccountControllerIT {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", notNullValue()))
                 .andExpect(jsonPath("$.balance").value(100))
+                .andExpect(jsonPath("$.currency").value("USD"));
+    }
+
+    @Test
+    void shouldDepositMoney() throws Exception {
+        String createJson = """
+        {
+          "amount": 100,
+          "currency": "USD"
+        }
+    """;
+
+        String response = mockMvc.perform(post("/accounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(createJson))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        String id = JsonPath.read(response, "$.id");
+
+        String depositJson = """
+        {
+          "amount": 50,
+          "currency": "USD"
+        }
+    """;
+
+        mockMvc.perform(post("/accounts/" + id + "/deposit")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(depositJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.balance").value(150))
+                .andExpect(jsonPath("$.currency").value("USD"));
+    }
+
+    @Test
+    void shouldWithdrawMoney() throws Exception {
+        String createJson = """
+        {
+          "amount": 100,
+          "currency": "USD"
+        }
+    """;
+
+        String response = mockMvc.perform(post("/accounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(createJson))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        String id = JsonPath.read(response, "$.id");
+
+        String withdrawJson = """
+        {
+          "amount": 40,
+          "currency": "USD"
+        }
+    """;
+
+        mockMvc.perform(post("/accounts/" + id + "/withdraw")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(withdrawJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.balance").value(60))
                 .andExpect(jsonPath("$.currency").value("USD"));
     }
 }
