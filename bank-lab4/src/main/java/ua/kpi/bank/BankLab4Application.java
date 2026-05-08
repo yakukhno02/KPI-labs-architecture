c74256b2f1875e7579aab115da5d69c7612e6b18
ykmshhh <yakukhno.maria@lll.kpi.ua>
2026-05-08 13:09:16 +0300

    lab4: feat: add async event handling

--- bank-lab4/src/main/java/ua/kpi/bank/BankLab4Application.java
+++ bank-lab4/src/main/java/ua/kpi/bank/BankLab4Application.java
@@ -2,8 +2,10 @@ package ua.kpi.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class BankLab4Application {
 
     public static void main(String[] args) {
